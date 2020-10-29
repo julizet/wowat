e94429418f73f53a845329b349451253cb0e51e3
Julia Zet <julizet@users.noreply.github.com>
2020-10-29 18:15:33 +0100

    tweak GoogleStrategySettings to enable heroku proxy

--- services/passport.js
+++ services/passport.js
@@ -27,6 +27,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       //search if users already exists
